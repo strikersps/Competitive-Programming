91d0e0f0b8538ffdc8e4dcb588ebfb9457650449
Harsh Garg <43584099+invictus1599@users.noreply.github.com>
2019-10-23 13:53:18 +0530

    Create MEDEL.cpp

---
+++ Code-Chef/MEDEL.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+int del(int a[],int x,int n){
+	int i;
+	if(x<=n-2){
+		for(i=x;i<n;i++)
+			a[i]=a[i+1];
+	}
+}
+int main() {
+	int t;
+	cin>>t;
+	while(--t>=0){
+	    int n,i,t;
+	    cin>>n;
+	    int a[n];
+	    t=n;
+	    for(i=0;i<n;i++)cin>>a[i];
+	    while(t>=3){
+	    	if(a[0]>=a[1] && a[0]>=a[2]){
+	    		if(a[1]>=a[2])
+	    			del(a,1,t);
+	    		else
+	    			del(a,2,t);
+			}
+			else if(a[1]>=a[2]){
+				if(a[0]>=a[2])
+					del(a,0,t);
+				else
+					del(a,2,t);
+			}
+			else{
+				if(a[1]>=a[0])
+					del(a,1,t);
+				else
+					del(a,0,t);
+			}
+			t--;
+		}
+	    for(i=0;i<2;i++)cout<<a[i]<<" ";
+		cout<<endl;
+	}
+	return 0;
+}
