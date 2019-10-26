1fef4d0fdc41d8ef859e01d1c363375ff2f2e03f
strikersps <sharma.suraj5666@gmail.com>
2019-10-27 02:21:53 +0530

    Solved 10001st Prime

---
+++ Project-Euler/10001st-Prime-Number/10001st_prime_number.c
@@ -0,0 +1,45 @@
+/*  Problem Statement: https://www.hackerrank.com/contests/projecteuler/challenges/euler007/problem
+    Author: striker
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<inttypes.h>
+#include<stdbool.h>
+#include<string.h>
+#include<assert.h>
+
+#define MAX_LIMIT 1000000 // Chosen this limit because there are 78,493 prime number <= 10^6
+#define TOTAL_PRIMES 78493
+uint32_t prime_number_table[TOTAL_PRIMES]; // Making the table global so that I can access it throughout the program.
+
+void generate_prime(void);
+
+int main(void) {
+    uint32_t test;
+    scanf("%"SCNu32, &test);
+    assert(test > 0 && test < 1001);
+    generate_prime();
+    while(test--) {
+        uint32_t n;
+        scanf("%"SCNu32, &n);
+        assert(n > 0 && n < TOTAL_PRIMES);
+        printf("%"PRIu32"\n", prime_number_table[n - 1]); // Constant time operation.
+    }
+    return EXIT_SUCCESS;
+}
+
+void generate_prime(void) {
+    bool sieve_table[MAX_LIMIT];
+    memset(sieve_table,true,(sizeof(bool) * MAX_LIMIT));
+    sieve_table[0] = false;
+    sieve_table[1] = false;
+    for(uint32_t i = 2, k = 0; i < MAX_LIMIT; ++i) {
+        if(sieve_table[i]) {
+            prime_number_table[k++] = i;
+            for(uint32_t j = (i * i); j <= MAX_LIMIT; j += i) {
+                sieve_table[j] = false;
+            }
+        }
+    }
+}
---
+++ Project-Euler/10001st-Prime-Number/10001st_prime_number.py
@@ -0,0 +1,32 @@
+'''
+Problem Statement: https://www.hackerrank.com/contests/projecteuler/challenges/euler007/problem
+Author: striker
+'''
+
+MAX_LIMIT = 10**6
+
+def prime_number_generator():
+    sieve_table = [True] * MAX_LIMIT
+    sieve_table[0] = False
+    sieve_table[1] = False
+    prime_numbers = list()
+    for i in range(2,MAX_LIMIT):
+        if sieve_table[i]:
+            prime_numbers.append(i)
+            j = i**2
+            while j < MAX_LIMIT:
+                sieve_table[j] = False
+                j += i
+    return prime_numbers
+
+def main():
+    test = int(input())
+    assert(test > 0)
+    prime_numbers = prime_number_generator()
+    for _ in range(test):
+        n = int(input())
+        print(prime_numbers[n - 1])
+
+
+if __name__ == "__main__":
+    main()
