f141b0ea4828fb4b01f41288c5f4bb84fc998ca0
strikersps <sharma.suraj5666@gmail.com>
2019-11-01 03:14:14 +0530

    Solved Bytelandian Gold Coins

--- SPOJ/COINS/bytelandian_gold_coins.c
+++ SPOJ/COINS/bytelandian_gold_coins.c
@@ -16,13 +16,14 @@ ull_t max_american_dollars(ull_t);
 ull_t find_max(ull_t,ull_t);
 
 int main(void) {
-    int iteration = 10;
-    while(iteration--) {
+    for(int i = 0; i < 10; ++i) {
         ull_t n;
         scanf("%llu", &n);
         assert(n >= 0 && n < 1000000001);
         cache = calloc(n,sizeof(ull_t));
         printf("%llu\n", max_american_dollars(n));
+        free(cache);
+        cache = NULL; // To get rid of dangling pointer.
     }
     return EXIT_SUCCESS;
 }
@@ -30,19 +31,12 @@ int main(void) {
 ull_t max_american_dollars(ull_t n) {
     if(n == 0) {
         return 0;
-    } else if(n == 1) {
-        cache[n] = 1;
-    } else if(n == 2) {
-        cache[n] = 2;
-    } else if(n == 3) {
-        cache[n] = 3;
-    } else if(n == 4) {
-        cache[n] = 4;
-    } else if(!cache[n]) {
-        cache[n] = find_max(n,(max_american_dollars(n >> 1) + max_american_dollars(n / 3) + max_american_dollars(n / 4)));
-        return cache[n];
+    } else if(n == 1 || n == 2 || n == 3 || n == 4) {
+        cache[n - 1] = n;
+    } else if(!cache[n - 1]) {
+        cache[n - 1] = find_max(n,(max_american_dollars(n >> 1) + max_american_dollars(n / 3) + cache[((n >> 1) >> 1) - 1]));
     }
-    return cache[n];
+    return cache[n - 1];
 }
 
 ull_t find_max(ull_t a,ull_t b) {
---
+++ SPOJ/COINS/test-cases/test_case_generation.c
@@ -0,0 +1,40 @@
+/*  * Program to generate the test-cases for the problem: https://www.spoj.com/problems/COINS/
+    * Program is based on command-line arguments which takes a filename as an argument.
+    * Author: striker
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<inttypes.h>
+#include<time.h>
+#include<assert.h>
+
+#ifdef _WIN32
+    #include<Windows.h>
+#else
+    #include<unistd.h>
+#endif
+
+typedef unsigned long long ull_t;
+ull_t MAX_LIMIT = 100;
+
+int main(int argc,char *const argv[]) {
+    srand(time(NULL));
+    if(argc == 2) {
+        FILE *test_file = fopen(argv[1],"w");
+        if(test_file) {
+            sleep(1);
+            srand(time(NULL));
+            for(int i = 0; i < 10; ++i) {
+                fprintf(test_file,"%llu\n",(rand() % MAX_LIMIT));
+            }
+        } else {
+            fprintf(stderr,"File not opened successfully\n");
+            return EXIT_FAILURE;
+        }
+    } else {
+        fprintf(stderr,"Insufficient or More number of command line arguments\n");
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
