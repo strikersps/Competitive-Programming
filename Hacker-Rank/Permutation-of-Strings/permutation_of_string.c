2e3935aca04b181fcdc8b7e12c97de392fadb4bc
strikersps <spsdeveloper786@gmail.com>
2019-11-18 17:20:27 +0530

    Made some changes in the source-code of permutation of string.c file

--- Hacker-Rank/Permutation-of-Strings/permutation_of_string.c
+++ Hacker-Rank/Permutation-of-Strings/permutation_of_string.c
@@ -10,8 +10,7 @@
 #include <stdbool.h>
 
 // Print the next lexicographic permutation of the given string if it exists and return true else false.
-int next_permutation(int n, char **s)
-{
+int next_permutation(int n, char **s) {
     int i,j;
     if(n == 0) {
         return false;
@@ -40,18 +39,17 @@ int main(void)
 	int n; // Total number of strings.
 	scanf("%d", &n);
 	s = calloc(n, sizeof(char*)); // Creating an array of character pointers and initialised it with NULL.
-	for (int i = 0; i < n; i++)
-	{
+	for (int i = 0; i < n; i++) {
 		s[i] = calloc(11, sizeof(char)); // Storing the base address of an array containing 11 location. Every location will store a character constant.
 		scanf("%s", s[i]); // Taking input string.
 	}
-	do
-	{
+	do {
 		for (int i = 0; i < n; i++)
 			printf("%s%c", s[i], i == n - 1 ? '\n' : ' '); // Print the string seperated by space until i == n.
 	} while (next_permutation(n, s)); // The set of instructions in do {} will execute until the next_permutaion returns false i.e when there is no next lexicographic permutation available.
-	for (int i = 0; i < n; i++) // Free the memory pointed by each of the pointers in the array.
+	for (int i = 0; i < n; i++) { // Free the memory pointed by each of the pointers in the array.
 		free(s[i]);
+    }
 	free(s); // Free char **s.
 	return 0;
 }
