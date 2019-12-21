1aeb3c64dc21b118864b6c668ecf241d8b4aae21
strikersps <spsdeveloper786@gmail.com>
2019-12-21 15:39:19 +0530

    Solved Reverse The Number in C++

---
+++ Code-Chef/Reverse-The-Number/reverse_the_number.cpp
@@ -0,0 +1,28 @@
+/*  Problem Statement: https://www.codechef.com/problems/FLOW007
+    Author: striker
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+static unsigned int compute_reverse_number(unsigned int);
+
+int main(void) {
+    int test;
+    std :: cin >> test;
+    while(test--) {
+        unsigned int n;
+        std :: cin >> n;
+        std :: cout << compute_reverse_number(n) << endl;
+    }
+    return 0;
+}
+
+static unsigned int compute_reverse_number(unsigned int n) {
+    unsigned int reverse_number = 0;
+    while(n) {
+        reverse_number = (10 * reverse_number) + (n % 10);
+        n /= 10;
+    }
+    return reverse_number;
+}
