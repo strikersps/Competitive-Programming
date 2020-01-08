78ed089cf84ca51a8a3c2f99706c2ece7aaa3227
strikersps <spsdeveloper786@gmail.com>
2020-01-08 18:19:09 +0530

    Updated code for alternating subarray prefix

--- Code-Chef/Alternating-Subarray-Prefix/alternating_subarray_prefix.c
+++ Code-Chef/Alternating-Subarray-Prefix/alternating_subarray_prefix.c
@@ -12,9 +12,13 @@ typedef struct sequence {
     uint32_t len_of_longest_alt_seq;
 } sequence_t;
 
-void find_longest_alternating_seq(sequence_t *const,uint32_t);
+void find_longest_alternating_seq(sequence_t *const, uint32_t);
 
 int main(void) {
+    #ifndef ONLINE_JUDGE
+        freopen("test-cases/test-case-1.in", "r", stdin);
+        freopen("test-cases/test-case-1.out", "w", stdout);
+    #endif
     uint8_t test;
     scanf("%"SCNu8, &test);
     assert(test > 0 && test < 11);
@@ -22,13 +26,13 @@ int main(void) {
         uint32_t n;
         scanf("%"SCNu32, &n);
         assert(n > 0 && n < 100001);
-        sequence_t *const sequence = calloc(n,sizeof(sequence_t));
+        sequence_t *const sequence = calloc(n, sizeof(sequence_t));
         if(sequence) {
             for(uint32_t i= 0; i < n; ++i) {
-                scanf("%"SCNu32, &sequence[i].data);
+                scanf("%"SCNd32, &sequence[i].data);
                 sequence[i].len_of_longest_alt_seq = 1;
             }
-            find_longest_alternating_seq(sequence,n);
+            find_longest_alternating_seq(sequence, n);
             for(uint32_t i = 0; i < n; ++i) {
                 printf("%"PRIu32" ", sequence[i].len_of_longest_alt_seq);
             }
@@ -41,7 +45,7 @@ int main(void) {
     return EXIT_SUCCESS;
 }
 
-void find_longest_alternating_seq(sequence_t *const sequence,uint32_t n) {
+void find_longest_alternating_seq(sequence_t *const sequence, uint32_t n) {
     for(int32_t i = (n - 2); i >= 0; --i) {
         if((sequence[i].data > 0 && sequence[i + 1].data < 0) || (sequence[i].data < 0 && sequence[i + 1].data > 0)) {
             sequence[i].len_of_longest_alt_seq = sequence[i + 1].len_of_longest_alt_seq + 1;
