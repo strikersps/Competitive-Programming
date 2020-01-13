3f4590da4bdcbcdd0af5bee290fc494918fdf8ee
strikersps <spsdeveloper786@gmail.com>
2020-01-13 20:04:03 +0530

    Solved https://www.codechef.com/JAN20B/problems/CHFDORA

--- Code-Chef/Chef-and-Doraemon/chef_and_doraemon.c
+++ Code-Chef/Chef-and-Doraemon/chef_and_doraemon.c
@@ -1,21 +1,44 @@
-/*  Problem Statement: https://www.codechef.com/JAN20B/problems/CHFDORA
- *  Author: striker
-*/
+/*
+ * Problem Statement: https://www.codechef.com/problems/CHFDORA
+ * Author: striker
+ *
+ * Copyright 2020 striker
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <limits.h>
+#include <time.h>
 
 typedef unsigned long long ull_t;
 typedef long long ll_t;
 
-#define MEMORY_ALLOCATION_FAILED_ERROR(variable, bytes) fprintf(stderr, "Line number: %u: Not able to allocate <%lu> bytes of memory for <%s> variable.\n", __LINE__, (bytes), #variable)
-#define CONSTRAINTS_OUT_OF_BOUND_ERROR(variable, constraints) fprintf(stderr, "Line number: %u: Constraints not satisfied for the <%s> variable, i.e. %s.\n", __LINE__, #variable, #constraints)
-#define INITIALISE_CONTAINER(container, bytes) memset(container, 0, (bytes));
+typedef struct Point2D {
+    int x, y;
+} point_2d;
+
+#define MOD (1000000000 + 7) // Prime Number
+#define PI 3.141592653589793 // Number of digits(15) of Pi which JPL uses for Interplanetary Caculations.
+
+#define MEMORY_ALLOCATION_FAILED_ERROR(variable, bytes) fprintf(stderr, "Line number: %u: Not able to allocate <%lu> bytes of memory to <%s> variable.\n", __LINE__, (bytes), #variable)
+#define CONSTRAINTS_OUT_OF_BOUND_ERROR(variable, constraints) fprintf(stderr, "Line number: %u: Constraints not satisfied for the variable <%s>, i.e. %s.\n", __LINE__, #variable, #constraints)
+#define INITIALISE_CONTAINER_ZERO(container, bytes) memset(container, 0, (bytes))
 #define FIND_MAX(a, b) (a) > (b) ? (a) : (b)
 #define FIND_MIN(a, b) (a) < (b) ? (a) : (b)
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#define FIND_MID(start, end) (((end) - (start)) >> 1) + (start)
 
 static ull_t compute_number_of_valid_pairs(int **const, const int, const int);
 static void free_memory(int **const, const int);
