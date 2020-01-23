389336d6489136f8bbc2b8c61f27c796945d8829
strikersps <spsdeveloper786@gmail.com>
2020-01-24 00:36:40 +0530

    Solved https://www.hackerrank.com/challenges/no-idea/problem

--- Hacker-Rank/Python-Programming-Track/No-Idea/no_idea.py
+++ Hacker-Rank/Python-Programming-Track/No-Idea/no_idea.py
@@ -3,21 +3,18 @@ Problem Statement: https://www.hackerrank.com/challenges/no-idea/problem
 Author: striker
 '''
 
-def compute_total_happiness(n,set_a,set_b):
+def compute_total_happiness(number_list, set_a, set_b):
     total_happiness = 0
-    for data in n:
-        if data in set_a:
+    for number in number_list:
+        if number in set_a:
             total_happiness += 1
-        elif data in set_b:
+        elif number in set_b:
             total_happiness -= 1
     return total_happiness
 
 def main():
-    n,m = tuple(map(int,input().split()))
-    list_of_integers = list(map(int,input().split()))
-    set_a = set(map(int,input().split()))
-    set_b = set(map(int,input().split()))
-    print(compute_total_happiness(list_of_integers,set_a,set_b))
+    n, m = tuple(map(int,input().split()))
+    print("{}".format(compute_total_happiness(list(map(int, input().rstrip().split())), set(map(int, input().rstrip().split())), set(map(int, input().rstrip().split())))))
 
 if __name__ == "__main__":
     main()
