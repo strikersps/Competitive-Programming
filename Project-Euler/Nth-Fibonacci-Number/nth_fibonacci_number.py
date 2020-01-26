2bff58d865b59c0d73bb61447366a6fc0a3683b2
strikersps <spsdeveloper786@gmail.com>
2020-01-27 03:16:48 +0530

    Solved Nth-Fibonacci-Number

--- Project-Euler/Nth-Fibonacci-Number/nth_fibonacci_number.py
+++ Project-Euler/Nth-Fibonacci-Number/nth_fibonacci_number.py
@@ -5,10 +5,12 @@ Author: striker
 
 import math
 
+GOLDEN_RATIO = 1.61803398875
+
 def main():
     for test in range(int(input().rstrip())):
         n = int(input().rstrip())
-        print("{}".format(int(math.ceil((n - 1 + (math.log10(5) * 0.5)) / math.log10(1.61803398874)))))
+        print("{}".format(int(math.ceil((n - 1 + (math.log10(5) * 0.5)) / math.log10(GOLDEN_RATIO)))))
 
 if __name__ == "__main__":
     main()
