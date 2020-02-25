b467a0086ccc0eabd003a58008e2b1128080b4f5
strikersps <spsdeveloper786@gmail.com>
2020-02-25 23:56:16 +0530

    Solved Reverse Words In a String

---
+++ Leet-Code/Reverse-Words-In-A-String/reverse_words_in_sentence.c
@@ -0,0 +1,125 @@
+/*
+ * Problem Statement: https://leetcode.com/problems/reverse-words-in-a-string/
+ * Author: striker
+ *
+ * Copyright 2020 striker
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include <limits.h>
+#include <time.h>
+
+typedef unsigned long long ull_t;
+typedef long long ll_t;
+
+typedef struct Point2D {
+    int x, y;
+} point_2d;
+
+#define MOD (1000000000 + 7) // Prime Number
+#define PI 3.141592653589793 // Number of digits(15) of Pi which JPL uses for Interplanetary Caculations.
+#define GOLDEN_RATIO 1.618033988749895 // Number of digits(15).
+
+#define MEMORY_ALLOCATION_FAILED_ERROR(variable, bytes) fprintf(stderr, "Line number: %u: Not able to allocate <%lu> bytes of memory to <%s> variable.\n", __LINE__, (bytes), #variable)
+#define CONSTRAINTS_OUT_OF_BOUND_ERROR(variable, constraints) fprintf(stderr, "Line number: %u: Constraints not satisfied for the variable <%s>, i.e. %s.\n", __LINE__, #variable, #constraints)
+#define SCANF_READ_ERROR(return_val) fprintf(stderr, "Line number: %u: scanf() read error!\nExpected-Return-Value: %d\n", __LINE__, return_val); exit(0)
+#define INITIALISE_INT_CONTAINER_ZERO(container, bytes) memset(container, 0, (bytes))
+#define INITIALISE_CHAR_CONTAINER_ZERO(container, bytes) memset(container, '0', (bytes))
+#define FIND_MAX(a, b) (a) > (b) ? (a) : (b)
+#define FIND_MIN(a, b) (a) < (b) ? (a) : (b)
+#define FIND_MID(start, end) (((end) - (start)) >> 1) + (start)
+
+// The below function macros refers to the GCC functions for doing computation directly on the bit-level of a number.
+#define COMPUTE_SET_BITS(number) __builtin_popcountll(number) // Returns the number of set-bits in number (unsigned long long).
+#define COMPUTE_PARITY(number) __builtin_parityll(number) // Returns the parity of the number (unsigned long long) i.e. True if 1's are odd else False.
+#define COUNT_LEAD_ZEROES(number) __builtin_clzll(number) // Returns the count of lead zeroes before first set-bit from MSB in number (unsigned long long).
+#define COUNT_TRAIL_ZEROES(number) __builtin_ctzll(number) // Return the count of trailing zeroes in number(unsigned long long).
+
+#define MAX_STRING_LENGTH 100001
+
+static void reverse_word_in_sentence(char *const);
+static void reverse_word(char *const, int, int);
+static void remove_multiple_spaces(char *const);
+
+int main(void) {
+    #ifndef ONLINE_JUDGE
+        freopen("test-cases/test-case-1.in", "r", stdin);
+        freopen("test-cases/test-case-1.out", "w", stdout);
+    #endif
+    int test;
+    if(1 == scanf("%d", &test)) {
+        if(test < 1) {
+            CONSTRAINTS_OUT_OF_BOUND_ERROR(test, test value cannot be 0 or -ve);
+            exit(0);
+        }
+        getchar_unlocked(); // To clean the buffer before fgets().
+        while(test--) {
+            char sentence[MAX_STRING_LENGTH];
+            fgets(sentence, MAX_STRING_LENGTH, stdin);
+            char *const enter_loc = strchr(sentence, '\n');
+            if(enter_loc) {
+                *enter_loc = '\0';
+            }
+            reverse_word_in_sentence(sentence);
+            puts(sentence);
+
+        }
+    } else {
+        SCANF_READ_ERROR(1);
+    }
+    return EXIT_SUCCESS;
+}
+
+static void remove_multiple_spaces(char *const sentence) {
+    for(register int i = strlen(sentence) - 1; i >= 0 && isspace(sentence[i]); sentence[i--] = '\0');
+    int extra_whitespace_count = 0;
+    for(register int i = 0; isspace(sentence[i]); ++extra_whitespace_count, ++i);
+    register int i;
+    for(i = extra_whitespace_count; sentence[i] != '\0'; ++i) {
+        sentence[i - extra_whitespace_count] = sentence[i];
+        if(isspace(sentence[i]) && isspace(sentence[i + 1])) {
+            ++extra_whitespace_count;
+        }
+    }
+    for(i -= extra_whitespace_count; sentence[i] != '\0'; sentence[i++] = '\0');
+}
+
+static void reverse_word(char *const sentence, int start, int end) {
+    while(start < end) {
+        char temp = sentence[start];
+        sentence[start++] = sentence[end];
+        sentence[end--] = temp;
+    }
+}
+
+static void reverse_word_in_sentence(char *const sentence) {
+    for(register int i = 0, start = 0, end = -1; sentence[i] != '\0'; ++i) {
+        if(sentence[i] == ' ' && sentence[i + 1] != ' ') {
+            start = i + 1;
+        } else if(sentence[i] != ' ' && (sentence[i + 1] == ' ' || sentence[i + 1] == '\0')) {
+            end = i;
+        }
+        if((start != (-1) && end != (-1)) && (start < end)) {
+            reverse_word(sentence, start, end);
+            start = end = -1;
+        }
+    }
+    reverse_word(sentence, 0, strlen(sentence) - 1);
+    remove_multiple_spaces(sentence);
+}
