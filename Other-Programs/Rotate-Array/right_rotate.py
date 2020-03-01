891ad12ef1230ba2aad6cea1b7190aee577ec61f
strikersps <spsdeveloper786@gmail.com>
2020-03-02 00:36:49 +0530

    Solved Right Rotate Array

---
+++ Other-Programs/Rotate-Array/right_rotate.py
@@ -0,0 +1,15 @@
+'''
+Problem Statement: Refer to the readme.md file.
+Author: striker
+'''
+
+def right_rotate_list(sequence, k):
+    return " ".join(sequence[: k: ][: : -1] + sequence[k : len(sequence):][: : -1])
+
+def main():
+     for test in range(int(input().rstrip())):
+         n, k = tuple(map(int, input().rstrip().split()))
+         print("{}".format(right_rotate_list(input().rstrip().split()[: : -1], k)))
+
+if __name__ == "__main__":
+    main()
