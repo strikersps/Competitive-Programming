bb2890118ebd0198d989854c7b13ee106bd2cbfe
strikersps <spsdeveloper786@gmail.com>
2020-03-02 01:16:59 +0530

    Solved Rotate Arary to Right By K-Steps

--- Other-Programs/Rotate-Array/right_rotate.py
+++ Other-Programs/Rotate-Array/right_rotate.py
@@ -9,8 +9,7 @@ def right_rotate_list(sequence, k):
 def main():
      for test in range(int(input().rstrip())):
          n, k = tuple(map(int, input().rstrip().split()))
-         k %= n
-         print("{}".format(right_rotate_list(input().rstrip().split()[: : -1], k)))
+         print("{}".format(right_rotate_list(input().rstrip().split()[: : -1], k % n)))
 
 if __name__ == "__main__":
     main()
