98757c2dc76a9dab223ac280ac30013f07c69534
strikersps <spsdeveloper786@gmail.com>
2020-03-11 00:56:01 +0530

    Solved String Mutations

---
+++ Hacker-Rank/Swap-Case/Mutations/mutation_another_sol.py
@@ -0,0 +1,15 @@
+'''
+Problem Statement: https://www.hackerrank.com/challenges/python-mutations/problem
+Author: striker
+'''
+
+def mutate_string(string, position, replacement_char):
+    return string[: position] + replacement_char + string[position: ]
+
+def main():
+    string = input().rstrip()
+    position, replacement_char = input().rstrip().split()
+    print("{0}".format(mutate_string(string, int(position), replacement_char)))
+
+if __name__ == "__main__":
+    main()
---
+++ Hacker-Rank/Swap-Case/Mutations/mutations.py
@@ -0,0 +1,17 @@
+'''
+Problem Statement: https://www.hackerrank.com/challenges/python-mutations/problem
+Author: striker
+'''
+
+def mutate_string(string, position, replacement_char):
+    string_list = list(string) # Converted the list into string so that I can change the characters of the string as list are mutable objects, string is not.
+    string_list[position] = replacement_char
+    return "".join(string_list)
+
+def main():
+    s = input().strip()
+    position, exchange_char = input().rstrip().split()
+    print("{0}".format(mutate_string(s, int(position), exchange_char)))
+
+if __name__ == "__main__":
+    main()
