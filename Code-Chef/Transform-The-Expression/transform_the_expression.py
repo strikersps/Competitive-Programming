48e04cb6d9f9ea3b85236f3430bee9a0e23e3e56
strikersps <spsdeveloper786@gmail.com>
2020-03-28 22:22:22 +0530

    Solved Transform The Expression

--- Code-Chef/Transform-The-Expression/transform_the_expression.py
+++ Code-Chef/Transform-The-Expression/transform_the_expression.py
@@ -3,20 +3,27 @@ Problem Statement: https://www.codechef.com/problems/ONP
 Author: striker
 '''
 
+precedence_dict = {'^' : 5, '/' : 4, '*' : 3, '-' : 4, '+' : 1}
+
 def transform_to_rpn(expression):
     rpn_form = list()
     stack = list()
-    for index in range(len(expression)):
-        if expression[index] in ['(','+','-','*','^','/','%']:
-            stack.append(expression[index])
+    for token in expression:
+        if token == '(':
+            stack.append(token)
             continue
-        if expression[index] == ')':
+        if token in ['+', '-', '*', '^', '/']:
+            while stack and stack[-1] != '(' and precedence_dict[stack[-1]] > precedence_dict[token]:
+                rpn_form.append(stack.pop())
+            stack.append(token)
+            continue
+        if token == ')':
             optr = stack.pop()
             while optr != '(':
                 rpn_form.append(optr)
                 optr = stack.pop()
             continue
-        rpn_form.append(expression[index])
+        rpn_form.append(token)
     while stack:
         rpn_form.append(stack.pop())
     return rpn_form
