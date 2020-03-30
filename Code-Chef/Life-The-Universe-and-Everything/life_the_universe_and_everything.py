a868a7f1bdee4db03bbd96279e5782b59ec00f67
strikersps <spsdeveloper786@gmail.com>
2020-03-30 13:18:21 +0530

    Solved Life The Univer and Everything

---
+++ Code-Chef/Life-The-Universe-and-Everything/life_the_universe_and_everything.py
@@ -0,0 +1,13 @@
+'''
+Problem Statement: https://www.codechef.com/LRNDSA01/problems/TEST
+Author: striker
+'''
+
+def main():
+    n = int(input().strip())
+    while n != 42:
+        print("{0}".format(n))
+        n = int(input().strip())
+
+if __name__ == "__main__":
+    main()
