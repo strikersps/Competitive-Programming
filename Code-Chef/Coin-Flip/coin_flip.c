3dc77787dba0846a7f12e0a9a5584eac19ff7752
strikersps <spsdeveloper786@gmail.com>
2020-03-30 14:50:19 +0530

    Solved Coin Flip

--- Code-Chef/Coin-Flip/coin_flip.c
+++ Code-Chef/Coin-Flip/coin_flip.c
@@ -29,51 +29,63 @@ typedef long long ll_t;
 
 typedef struct Point2D {
     int x, y;
-} point_2d;
+} point_2d_t;
 
 #define MOD (1000000000 + 7) // Prime Number
 #define PI 3.141592653589793 // Number of digits(15) of Pi which JPL uses for Interplanetary Caculations.
+#define GOLDEN_RATIO 1.618033988749895 // Number of digits(15).
 
 #define MEMORY_ALLOCATION_FAILED_ERROR(variable, bytes) fprintf(stderr, "Line number: %u: Not able to allocate <%lu> bytes of memory to <%s> variable.\n", __LINE__, (bytes), #variable)
 #define CONSTRAINTS_OUT_OF_BOUND_ERROR(variable, constraints) fprintf(stderr, "Line number: %u: Constraints not satisfied for the variable <%s>, i.e. %s.\n", __LINE__, #variable, #constraints)
-#define INITIALISE_CONTAINER_ZERO(container, bytes) memset(container, 0, (bytes))
+#define SCANF_READ_ERROR(expected_return_val) fprintf(stderr, "Line number: %u: scanf() read error!\nExpected-Return-Value: %d.\n", __LINE__, expected_return_val); exit(0)
+#define STREAM_LINK_ERROR(file_path, stream_name) fprintf(stderr, "Line number: %u: Stream Link Error! Not able to link <%s> file to <%s> stream.\n", __LINE__, #file_path, #stream_name); exit(0);
+#define INITIALISE_INT_CONTAINER_ZERO(container, bytes) memset(container, 0, (bytes))
+#define INITIALISE_CHAR_CONTAINER_ZERO(container, bytes) memset(container, '0', (bytes))
 #define FIND_MAX(a, b) (a) > (b) ? (a) : (b)
 #define FIND_MIN(a, b) (a) < (b) ? (a) : (b)
 #define FIND_MID(start, end) (((end) - (start)) >> 1) + (start)
 
-enum coin_state {HEAD = 1, TAIL = 2};
+// The below function macros refers to the GCC functions for doing computation directly on the bit-level of a number.
+#define COMPUTE_SET_BITS(number) __builtin_popcountll(number) // Returns the number of set-bits in number (unsigned long long).
+#define COMPUTE_PARITY(number) __builtin_parityll(number) // Returns the parity of the number (unsigned long long) i.e. True if 1's are odd else False.
+#define COUNT_LEAD_ZEROES(number) __builtin_clzll(number) // Returns the count of lead zeroes before first set-bit from MSB in number (unsigned long long).
+#define COUNT_TRAIL_ZEROES(number) __builtin_ctzll(number) // Return the count of trailing zeroes in number(unsigned long long).
+
+static int compute_answer(const int, const int, const int);
 
 int main(void) {
-    #ifndef ONLINE_JUDGE
-        freopen("test-cases/test-case-1.in", "r", stdin);
-        freopen("test-cases/test-case-1.out", "w", stdout);
-    #endif
     int test;
-    scanf("%d", &test);
+    if(1 != scanf("%d", &test)) {
+        SCANF_READ_ERROR(1);
+    }
     while(test--) {
-        int number_of_games_played;
-        scanf("%d", &number_of_games_played);
-        for(int i = 0; i < number_of_games_played; ++i) {
-            int initial_state, number_of_rounds, q;
-            scanf("%d%d%d", &initial_state, &number_of_rounds, &q);
-            if(!(number_of_rounds % 2)) {
-                printf("%d\n", number_of_rounds >> 1);
-            } else {
-                if(HEAD == initial_state) {
-                    if(HEAD == q) {
-                        printf("%d\n", number_of_rounds >> 1);
-                    } else {
-                        printf("%d\n", (number_of_rounds >> 1) + 1);
-                    }
-                } else {
-                    if(HEAD == q) {
-                        printf("%d\n", (number_of_rounds >> 1) + 1);
-                    } else {
-                        printf("%d\n", number_of_rounds >> 1);
-                    }
-                }
+        int games_played;
+        if(1 != scanf("%d", &games_played)) {
+            SCANF_READ_ERROR(1);
+        }
+        for(register int i = 0; i < games_played; ++i) {
+            int initial_state, tot_coins, is_head;
+            if(3 != scanf("%d%d%d", &initial_state, &tot_coins, &is_head)) {
+                SCANF_READ_ERROR(3);
             }
+            printf("%d\n", compute_answer(initial_state, tot_coins, is_head));
         }
     }
     return EXIT_SUCCESS;
+}
+
+static int compute_answer(const int initial_state, const int tot_coins, const int is_head) {
+    if(!(tot_coins % 2)) {
+        return tot_coins >> 1;
+    }
+    if(initial_state == 1) {
+        if(is_head == 1) {
+            return tot_coins >> 1;
+        }
+        return 1 + (tot_coins >> 1);
+    }
+    if(is_head == 1) {
+        return 1 + (tot_coins >> 1);
+    }
+    return tot_coins >> 1;
 }
