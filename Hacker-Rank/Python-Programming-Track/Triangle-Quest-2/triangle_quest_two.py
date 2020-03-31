41605ad04c7b8ab7d4c6d582736fe8612ed43d65
strikersps <spsdeveloper786@gmail.com>
2020-04-01 02:07:05 +0530

    Solved Triangle Quest 2

---
+++ Hacker-Rank/Python-Programming-Track/Triangle-Quest-2/triangle_quest_two.py
@@ -0,0 +1,2 @@
+for i in range(1,int(input())+1): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print([1, 121, 12321, 1234321, 123454321, 12345654321, 1234567654321, 123456787654321, 12345678987654321, 12345678910987654321][i - 1])
