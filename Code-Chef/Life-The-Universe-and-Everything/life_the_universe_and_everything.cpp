b7325fcb2f26e5a09285c7987cf84abe89aee389
strikersps <spsdeveloper786@gmail.com>
2020-03-31 14:21:58 +0530

    Solved Life The Universe And Everything

---
+++ Code-Chef/Life-The-Universe-and-Everything/life_the_universe_and_everything.cpp
@@ -0,0 +1,14 @@
+/*
+ * Problem Statement: https://www.codechef.com/LRNDSA01/problems/TEST
+ * Author: striker
+ * */
+
+#include <bits/stdc++.h>
+
+int main(void) {
+    int n;
+    while(std :: cin >> n && n != 42) {
+        std :: cout << n << std :: endl;
+    }
+    return 0;
+}
