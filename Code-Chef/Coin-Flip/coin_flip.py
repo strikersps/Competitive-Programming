f76e0cd5fa550989aae0ee3c5bcaa596e944be49
strikersps <spsdeveloper786@gmail.com>
2020-04-01 21:29:04 +0530

    Solved Coin Flip

--- Code-Chef/Coin-Flip/coin_flip.py
+++ Code-Chef/Coin-Flip/coin_flip.py
@@ -10,8 +10,8 @@ def main():
             initial_state, number_of_rounds, prediction = tuple(map(int, input().rstrip().split()))
             if not number_of_rounds % 2 or initial_state == prediction:
                 print("{}".format(number_of_rounds >> 1))
-            else:
-                print("{}".format((number_of_rounds >> 1) + 1))
+                continue
+            print("{}".format((number_of_rounds >> 1) + 1))
 
 if __name__ == "__main__":
     main()
