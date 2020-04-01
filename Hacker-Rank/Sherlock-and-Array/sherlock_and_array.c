2a7f74115c67796195554e0662a8f6b104744b0c
strikersps <spsdeveloper786@gmail.com>
2020-04-01 22:50:56 +0530

    Solved Sherlock and Array

--- Hacker-Rank/Sherlock-and-Array/sherlock_and_array.c
+++ Hacker-Rank/Sherlock-and-Array/sherlock_and_array.c
@@ -1,52 +1,100 @@
-/*  Problem Statement: https://www.hackerrank.com/challenges/sherlock-and-array/problem
-    Author: Suraj Sharma
-*/
+/*
+ * Problem Statement: https://www.hackerrank.com/challenges/sherlock-and-array/problem
+ * Author: striker
+ *
+ * Copyright 2020 striker
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <inttypes.h>
 #include <stdbool.h>
-#include <assert.h>
+#include <string.h>
+#include <math.h>
+#include <limits.h>
+#include <time.h>
 
-bool find_the_index(uint32_t*,uint32_t);
+typedef unsigned long long ull_t;
+typedef long long ll_t;
+
+typedef struct Point2D {
+    int x, y;
+} point_2d_t;
+
+#define MOD (1000000000 + 7) // Prime Number
+#define PI 3.141592653589793 // Number of digits(15) of Pi which JPL uses for Interplanetary Caculations.
+#define GOLDEN_RATIO 1.618033988749895 // Number of digits(15).
+
+#define MEMORY_ALLOCATION_FAILED_ERROR(variable, bytes) fprintf(stderr, "Line number: %u: Not able to allocate <%lu> bytes of memory to <%s> variable.\n", __LINE__, (bytes), #variable)
+#define CONSTRAINTS_OUT_OF_BOUND_ERROR(variable, constraints) fprintf(stderr, "Line number: %u: Constraints not satisfied for the variable <%s>, i.e. %s.\n", __LINE__, #variable, #constraints)
+#define SCANF_READ_ERROR(expected_return_val) fprintf(stderr, "Line number: %u: scanf() read error!\nExpected-Return-Value: %d.\n", __LINE__, expected_return_val); exit(0)
+#define STREAM_LINK_ERROR(file_path, stream_name) fprintf(stderr, "Line number: %u: Stream Link Error! Not able to link <%s> file to <%s> stream.\n", __LINE__, #file_path, #stream_name); exit(0);
+#define INITIALISE_INT_CONTAINER_ZERO(container, bytes) memset(container, 0, (bytes))
+#define INITIALISE_CHAR_CONTAINER_ZERO(container, bytes) memset(container, '0', (bytes))
+#define FIND_MAX(a, b) (a) > (b) ? (a) : (b)
+#define FIND_MIN(a, b) (a) < (b) ? (a) : (b)
+#define FIND_MID(start, end) (((end) - (start)) >> 1) + (start)
+
+// The below function macros refers to the GCC functions for doing computation directly on the bit-level of a number.
+#define COMPUTE_SET_BITS(number) __builtin_popcountll(number) // Returns the number of set-bits in number (unsigned long long).
+#define COMPUTE_PARITY(number) __builtin_parityll(number) // Returns the parity of the number (unsigned long long) i.e. True if 1's are odd else False.
+#define COUNT_LEAD_ZEROES(number) __builtin_clzll(number) // Returns the count of lead zeroes before first set-bit from MSB in number (unsigned long long).
+#define COUNT_TRAIL_ZEROES(number) __builtin_ctzll(number) // Return the count of trailing zeroes in number(unsigned long long).
+
+static ll_t compute_sum(int[], const int);
+static bool check_balance_sum(int[], const int);
 
 int main(void) {
-    uint32_t test;
-    scanf("%"SCNu32,&test);
-    assert(test > 0 && test < 11);
-    while(test--) {
-        uint32_t n,*number=NULL;
-        scanf("%"SCNu32,&n);
-        assert(n > 0 && n < 100001);
-        number = calloc(sizeof(uint32_t),n);
-        if(number) {
-            printf("%s",(find_the_index(number,n)?"YES\n":"NO\n"));
-            free(number);
-        } else {
-            fprintf(stderr,"Memory not allocated to number pointer!\n");
-        }
+    int test;
+    if(1 != scanf("%d", &test)) {
+        SCANF_READ_ERROR(1);
     }
-    return 0;
+    while(test--) {
+        int n;
+        if(1 != scanf("%d", &n)) {
+            SCANF_READ_ERROR(1);
+        }
+        int sequence[n];
+        for(register int i = 0; i < n; ++i) {
+            if(1 == scanf("%d", sequence + i)) {
+                continue;
+            }
+            SCANF_READ_ERROR(1);
+        }
+        printf("%s\n", check_balance_sum(sequence, n) ? "YES" : "NO");
+    }
+    return EXIT_SUCCESS;
 }
 
-bool find_the_index(uint32_t *number,uint32_t n) {
-    uint32_t sum = 0;
-    for(uint32_t i=0;i<n;++i) {
-        scanf("%"SCNu32,&number[i]);
-        assert(number[i] >= 0 && number[i] < 20001);
-        sum += number[i];
+static ll_t compute_sum(int sequence[], const int n) {
+    ll_t sequence_sum = 0;
+    for(register int i = 0; i < n; ++i) {
+        sequence_sum += sequence[i];
     }
-    uint32_t left_sum,right_sum;
-    bool is_exist = false;
-    left_sum = 0;
-    right_sum = sum;
-    for(uint32_t i=0;i<n;++i) {
-        right_sum -= number[i];
-        if(left_sum == right_sum) {
-            is_exist = true;
+    return sequence_sum;
+}
+
+static bool check_balance_sum(int sequence[], const int n) {
+    ll_t right_sum = compute_sum(sequence, n);
+    ll_t left_sum = 0;
+    bool is_balanced = false;
+    for(register int i = 0; i < n; ++i) {
+        if(left_sum == (right_sum - sequence[i])) {
+            is_balanced = true;
             break;
         }
-        left_sum += number[i];
+        left_sum += sequence[i];
+        right_sum -= sequence[i];
     }
-    return is_exist;
+    return is_balanced;
 }
