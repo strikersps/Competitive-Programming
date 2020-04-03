f9811969a9e45ed7beab30d67f02a5abfd32c000
strikersps <spsdeveloper786@gmail.com>
2020-04-03 18:06:59 +0530

    Solved Walter and Jesse

--- Code-Chef/Walter-And-Jesse/walter_and_jesse.cpp
+++ Code-Chef/Walter-And-Jesse/walter_and_jesse.cpp
@@ -4,25 +4,24 @@
 */
 
 #include <bits/stdc++.h>
-using namespace std;
 
-#define FAST_IO(value) ios :: sync_with_stdio(value); cin.tie(value)
+#define FAST_IO(value) std :: ios :: sync_with_stdio(value); std :: cin.tie(value)
 
 int main() {
     FAST_IO(0);
     int test;
-    cin >> test;
+    std :: cin >> test;
     while(test--) {
         int n;
-        cin >> n;
-        vector <int> team_a(n), team_b(n);
+        std :: cin >> n;
+        std :: vector <int> team_a(n), team_b(n);
         for(register int i=0; i < n; i++) {
-            cin >> team_a[i];
+            std :: cin >> team_a[i];
         }
         for(register int i = 0; i < n; i++) {
-            cin >> team_b[i];
+            std :: cin >> team_b[i];
         }
-        cout << (*max_element(team_a.begin(), team_a.end()) == *max_element(team_b.begin(), team_b.end()) ? "NO" : "YES") << endl;
+        std :: cout << (*max_element(team_a.begin(), team_a.end()) == *max_element(team_b.begin(), team_b.end()) ? "NO" : "YES") << std :: endl;
     }
     return 0;
 }
