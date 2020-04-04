e30a214a9ce521e1918017ea5c5026cd1c79dd09
strikersps <spsdeveloper786@gmail.com>
2020-04-05 01:32:38 +0530

    Solved Chef Chick

--- Code-Chef/Chef-Chick/chef_chick.cpp
+++ Code-Chef/Chef-Chick/chef_chick.cpp
@@ -2,24 +2,22 @@
  *  Author: striker
 */
 
-#include<iostream>
-#include<vector>
-#include<algorithm>
-
-using namespace std;
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 int main() {
-    ios::sync_with_stdio(false); cin.tie(0); // For Fast IO
+    std :: ios :: sync_with_stdio(false); std :: cin.tie(0); // For Fast IO
     int test;
     std::cin >> test;
     while(test--) {
         int n;
-        std::cin >> n;
-        std::vector<int> data(n);
+        std :: cin >> n;
+        std :: vector <int> data(n);
         for(unsigned int i = 0; i < data.size(); ++i) {
-            cin >> data[i];
+            std :: cin >> data[i];
         }
-        std::cout << *std::min_element(data.begin(), data.end()) << endl;
+        std::cout << *min_element(data.begin(), data.end()) << std :: endl;
     }
     return 0;
 }
