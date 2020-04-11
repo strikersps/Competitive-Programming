027f1f746bdba1e94c0d7f4347049ddcd07cbcce
strikersps <spsdeveloper786@gmail.com>
2020-04-11 12:51:25 +0530

    Solved Rearratnge Zeroes

--- Other-Programs/Rearrange-Zeroes/rearrangement_of_zeroes.py
+++ Other-Programs/Rearrange-Zeroes/rearrangement_of_zeroes.py
@@ -6,19 +6,18 @@ Author: striker
 def rearrange_zeroes(sequence):
     index = 0
     for data in sequence:
-        if not data:
-            continue
-        sequence[index] = data
-        index += 1
+        if data:
+            sequence[index] = data
+            index += 1
     while index < len(sequence):
         sequence[index] = 0
         index += 1
-    return " ".join(list(map(str, sequence)))
+    return " ".join(map(str, sequence))
 
 def main():
-    for test in range(int(input().rstrip())):
+    for test in range(int(input().strip())):
         n = int(input())
-        print("{}".format(rearrange_zeroes(list(map(int, input().rstrip().split())))))
+        print("{}".format(rearrange_zeroes(list(map(int, input().strip().split())))))
 
 if __name__ == "__main__":
     main()
