f07ef34e1111f974e8eca62e43df5a611c965078
strikersps <spsdeveloper786@gmail.com>
2020-04-28 00:20:16 +0530

    Solved Chef and Squares

---
+++ Code-Chef/Chef-and-Squares/chef_and_squares.cpp
@@ -0,0 +1,82 @@
+/*
+ * Problem Statement: https://www.codechef.com/LRNDSA05/problems/CHEFSQRS
+ * Author: striker
+ *
+ * Copyright 2020 striker
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+*/
+
+#include <iostream>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <string>
+#include <numeric>
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <cassert>
+
+typedef unsigned long long ull_t;
+typedef long long ll_t;
+
+#define FAST_IO(value) std :: ios :: sync_with_stdio(value); std :: cin.tie(NULL)
+#define MOD 1000000007 // Constant (prime number) used in most competitive programming problems to reduce the answer to a 32-bit integer.
+#define PI 3.141592653589793 // Number of digits(15) of Pi which JPL uses for Interplanetary Caculations.
+#define GOLDEN_RATIO 1.618033988749895 // Number of digits(15).
+
+// C and C++ % operator is not a modular arithmethic operator rather its a remainder operator which gives remainder when a / b;
+// Refer: https://stackoverflow.com/questions/11720656/modulo-operation-with-negative-numbers
+ll_t compute_mod(ll_t a, ll_t b) {
+    assert(b != 0); // Modulus is not defined when denominator is zero.
+    ll_t rem = a % b;
+    return rem < 0 ? b + rem : rem;
+}
+
+int main(void) {
+    FAST_IO(0);
+    int test;
+    std :: cin >> test;
+    while(test--) {
+        ll_t n;
+        std :: cin >> n;
+        ll_t x = -1;
+        bool is_exist = false;
+        for(int i = 1; i * i <= n; ++i) {
+            if(!(n % i) && (i != (n / i))) {
+                ll_t d1 = ((n / i) + i) >> 1;
+                ll_t d2 = ((n / i) - i) >> 1;
+                if((n + (d2 * d2)) == (d1 * d1)) {
+                    is_exist = true;
+                    if(x == -1) {
+                        x = d2;
+                        continue;
+                    }
+                    x = std :: min(x, d2);
+                }
+            }
+        }
+        if(is_exist) {
+            std :: cout << x * x << std :: endl;
+            continue;
+        }
+        std :: cout << x << std :: endl;
+    }
+    return 0;
+}
