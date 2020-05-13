2e96c0689cc9c51e000872731bb6b65ac721724d
strikersps <spsdeveloper786@gmail.com>
2020-05-13 21:35:13 +0530

    Solved Am-I-a-Fibonacii Number

--- Code-Chef/Am-I-a-Fibonacci-Number/check_fibonacci_number.py
+++ Code-Chef/Am-I-a-Fibonacci-Number/check_fibonacci_number.py
@@ -5,12 +5,12 @@ Author: striker
 
 '''
 Algorithm Explanation:
-1. Compute and store all the Fibonacci Numbers till 4788 because, 4788th fibonacii number is last number consist of 1000 digits.
-2. As fibonacci numbers is an stricly increasing sequence i.e. after pre-computation you will have a strictly increasing sequence, that means you can use Binary-Search for each test-case and if for the case the number is present in the list print "YES" otherwise "NO".
+1. Compute and store all the Fibonacci Numbers till 4788 because, 4788th Fibonacii number is last number which consist of 1000 digits.
+2. As Fibonacci numbers is an stricly increasing sequence i.e. after pre-computation you will have a strictly increasing sequence, that means you can use Binary-Search for each test-case and if for the case the number is present in the list print "YES" otherwise "NO".
 
 Time-Complexity: O(n) + O(log(n)) where n = size of the sequence, i.e. 4788.
 
-The approach above works efficiently because Python supports Big Integers but in C/C++ programming language you need to store all the Fibonacci numbers as a string and then perform the search for each test-case.
+The approach above works efficiently because Python supports Big Integers but in C and C++ programming language you need to store all the Fibonacci numbers as a string and then perform the search for each test-case.
 '''
 
 fibonacii_sequence = list()
