6eef78df4a9c77501b39664feb287d16def7c337
strikersps <spsdeveloper786@gmail.com>
2020-05-15 19:58:52 +0530

    Solved Erasing Zeroes

---
+++ Code-Forces/Erasing-Zeroes/erasing_zeroes.py
@@ -0,0 +1,11 @@
+"""
+Problem Statement: https://codeforces.com/problemset/problem/1303/A
+Author: striker
+"""
+
+def main():
+    for test in range(int(input().strip())):
+        print(input().strip('0').count('0'))
+
+if __name__ == "__main__":
+    main()
