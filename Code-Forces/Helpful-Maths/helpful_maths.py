03890383dd6425e8b5ebb5ccce69176e5b44f643
strikersps <spsdeveloper786@gmail.com>
2020-06-22 19:37:29 +0530

    Solved Helpful Maths

---
+++ Code-Forces/Helpful-Maths/helpful_maths.py
@@ -0,0 +1,10 @@
+"""
+Problem Statement: https://codeforces.com/contest/339/problem/A
+Author: striker
+"""
+
+def main():
+    print('+'.join(sorted(input().strip().split('+'))))
+
+if __name__ == "__main__":
+    main()
