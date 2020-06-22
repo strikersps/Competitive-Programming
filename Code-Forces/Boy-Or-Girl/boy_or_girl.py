84a4a7af9b962c47ed9e739fb623ae649db608db
strikersps <spsdeveloper786@gmail.com>
2020-06-22 21:56:15 +0530

    Solved Boy Or Girl

---
+++ Code-Forces/Boy-Or-Girl/boy_or_girl.py
@@ -0,0 +1,10 @@
+"""
+Problem Statement: https://codeforces.com/problemset/problem/236/A
+Author: striker
+"""
+
+def main():
+    print("IGNORE HIM!" if len(set(input().strip())) % 2 else "CHAT WITH HER!")
+
+if __name__ == "__main__":
+    main()
