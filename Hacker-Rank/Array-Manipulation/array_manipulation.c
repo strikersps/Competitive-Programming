fb837679e74ce687c4b4866c1666dbb7fb4f75e6
strikersps <spsdeveloper786@gmail.com>
2020-06-23 19:33:20 +0530

    Solved Array Manipulation

--- Hacker-Rank/Array-Manipulation/array_manipulation.c
+++ Hacker-Rank/Array-Manipulation/array_manipulation.c
@@ -8,15 +8,15 @@
 #include<assert.h>
 
 int main(void) {
-    uint32_t n,m,start,end,k;
+    uint32_t n, m, start, end, k;
     int64_t *sequence = NULL;
-    scanf("%"SCNu32"%"SCNu32, &n,&m);
+    scanf("%"SCNu32"%"SCNu32, &n, &m);
     assert((n > 2 && n < 10000001) && (m > 0 && m < 200001));
-    sequence = calloc(sizeof(int64_t),n);
+    sequence = calloc(n, sizeof*sequence);
     if(sequence) {
         int64_t max_val = 0;
         for(uint32_t i = 0; i < m; ++i) {
-            scanf("%"SCNu32"%"SCNu32"%"SCNu32, &start,&end,&k);
+            scanf("%"SCNu32"%"SCNu32"%"SCNu32, &start, &end, &k);
             assert((start > 0 && start <= end) && (end > 0 && end <= n));
 			sequence[--start] += k;
             if(end < n) {
@@ -25,12 +25,12 @@ int main(void) {
         }
         uint64_t val = 0;
         for(uint32_t i = 0; i < n; ++i) {
-            val = val + sequence[i];
+            val += sequence[i];
             if(val > max_val) {
                 max_val = val;
             }
         }
-        printf("%"PRIu64"\n",max_val);
+        printf("%"PRIu64"\n", max_val);
         free(sequence);
     } else {
         fprintf(stderr,"Memory not allocated to pointer name sequence!\n");
