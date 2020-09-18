24d59d532171a301b31e8ac87fc2b2e1f020656f
strikersps <spsdeveloper786@gmail.com>
2020-09-18 10:11:02 +0530

    Solved Matrix Multiplication

---
+++ Other-Programs/Matrix-Multiplication/matrix_multiplication.py
@@ -0,0 +1,38 @@
+"""
+Problem Statement: Refer to the readme.md file.
+Author: striker
+"""
+
+def display_matrix(matrix: list) -> None:
+    for row in matrix:
+        for data in row:
+            print(f'{data: 5}', end = ' ')
+        print()
+
+def compute_matrix_multiplication(matrixA: list, matrixB: list, n: int, m: int) -> list:
+    result_matrix = [[0] * m for _ in range(n)]
+    for row_index in range(n):
+        for col_index in range(m):
+            for k in range(len(matrixB)):
+                result_matrix[row_index][col_index] += (matrixA[row_index][k] * matrixB[k][col_index])
+    return result_matrix
+
+def main():
+    a, b = map(int, input().strip().split())
+    matrixA = [list(map(int, input().strip().split())) for row in range(a)]
+    print("Matrix-A:")
+    display_matrix(matrixA)
+    c, d = map(int, input().strip().split())
+    matrixB = [list(map(int, input().strip().split())) for row in range(c)]
+    print("Matrix-B:")
+    display_matrix(matrixB)
+    try:
+        assert(a == d) # Matrix Multiplication can only be performed if number of rows in matrix-A == number of cols in matrix-B.
+        print("Result-Matrix:")
+        display_matrix(compute_matrix_multiplication(matrixA, matrixB, a, d))
+    except AssertionError:
+        print("Basic Condition of Matrix-Multiplication is not Satisfied."
+        "\ni.e. Matrix Multiplication can only be perfomed if number of rows in matrix-A == number of cols in matrix-B.")
+
+if __name__ == "__main__":
+    main()
