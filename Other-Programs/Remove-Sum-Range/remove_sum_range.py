6cf700ed5e9f1bd382b941de5c459161afe410b0
strikersps <spsdeveloper786@gmail.com>
2020-09-20 01:50:40 +0530

    Solved Remove-Sum-Range

--- Other-Programs/Remove-Sum-Range/remove_sum_range.py
+++ Other-Programs/Remove-Sum-Range/remove_sum_range.py
@@ -4,11 +4,17 @@ Author: striker
 """
 
 def summer_69(sequence: list) -> int:
-    six_index = [index for index, num in enumerate(sequence) if num == 6]
-    nine_index = [index for index, num in enumerate(sequence) if num == 9]
-    sequence_sum = sum(sequence)
-    for id1, id2 in zip(six_index, nine_index):
-        sequence_sum -= sum(sequence[id1: id2 + 1])
+    sequence_sum = 0
+    six_found = False
+    for number in sequence:
+        if number == 6:
+            six_found = True
+            continue
+        if number == 9:
+            six_found = False
+            continue
+        if not six_found:
+            sequence_sum += number
     return sequence_sum
 
 def main():
