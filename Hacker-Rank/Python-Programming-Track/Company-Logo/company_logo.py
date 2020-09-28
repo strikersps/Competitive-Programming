48700cc100a1575a3f84b78aa74ddb57bea802a5
strikersps <spsdeveloper786@gmail.com>
2020-09-28 22:42:14 +0530

    Solved Company Logo

--- Hacker-Rank/Python-Programming-Track/Company-Logo/company_logo.py
+++ Hacker-Rank/Python-Programming-Track/Company-Logo/company_logo.py
@@ -5,18 +5,8 @@ Author: striker
 
 from collections import Counter
 
-def decide_logo(alphabet_freq: tuple) -> list:
-    is_all_freq_same = all(alphabet_freq[0][1] == t[1] for t in alphabet_freq)
-    if is_all_freq_same:
-        return sorted(alphabet_freq)
-    alphabet_freq = sorted(alphabet_freq, key = lambda x: x[1], reverse = True)
-    for index in range(len(alphabet_freq) - 1):
-        if alphabet_freq[index][1] == alphabet_freq[index + 1][1] and alphabet_freq[index][0] > alphabet_freq[index + 1][0]:
-            alphabet_freq[index], alphabet_freq[index + 1] = alphabet_freq[index + 1], alphabet_freq[index]
-    return alphabet_freq
-
 def main():
-    for key, value in decide_logo(tuple(Counter(input().strip()).items()))[:3]:
+    for key, value in sorted(Counter(input().strip()).most_common(), key = lambda x: (-x[1], x[0]))[:3]:
         print(f'{key} {value}')
 
 
