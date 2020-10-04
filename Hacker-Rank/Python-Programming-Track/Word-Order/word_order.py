d38bd9f6121be53866855b498b4dd81bfa79103c
strikersps <spsdeveloper786@gmail.com>
2020-10-04 11:26:30 +0530

    Added Solutions to Problems of Hacker-Rank Python Programming Track

--- Hacker-Rank/Python-Programming-Track/Word-Order/word_order.py
+++ Hacker-Rank/Python-Programming-Track/Word-Order/word_order.py
@@ -3,16 +3,13 @@ Problem Statement: https://www.hackerrank.com/challenges/word-order/problem
 Author: striker
 """
 
-from collections import OrderedDict, Counter
+from collections import OrderedDict
 
 def main():
     words = OrderedDict()
     for _ in range(int(input().strip())):
         word = input().strip()
-        if word not in words:
-            words[word] = 1
-            continue
-        words[word] += 1
+        words[word] = words.get(word, 0) + 1
     print(len(words))
     print(' '.join(str(value) for value in words.values()))
 
