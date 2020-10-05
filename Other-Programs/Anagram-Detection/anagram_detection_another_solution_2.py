56499cbbf9d0c40d36df526de41dd4e8f193bc54
strikersps <spsdeveloper786@gmail.com>
2020-10-06 00:44:20 +0530

    Solved Anagram Detection

--- Other-Programs/Anagram-Detection/anagram_detection_another_solution_2.py
+++ Other-Programs/Anagram-Detection/anagram_detection_another_solution_2.py
@@ -7,6 +7,9 @@ from functools import reduce
 from operator import mul
 
 def check_anagram(string1, string2):
+    """
+    NOTE: The following solution only works if the given two strings are made of valid lowercase english alphabets.
+    """
     prime_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
     get_prime_product = lambda string: reduce(mul, [prime_numbers[ord(character) - 97] for character in string], 1)
     return get_prime_product(string1) == get_prime_product(string2)
