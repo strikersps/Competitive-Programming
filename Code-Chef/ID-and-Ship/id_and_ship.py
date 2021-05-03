2a2dde8e8dfb16549c203b3a849ebdfa65223c8e
strikersps <spsdeveloper786@gmail.com>
2021-05-03 12:02:34 +0530

    Changed ID and Ship Python Solution

--- Code-Chef/ID-and-Ship/id_and_ship.py
+++ Code-Chef/ID-and-Ship/id_and_ship.py
@@ -5,12 +5,11 @@ Author: striker
 
 def main():
     data = {'B': 'BattleShip','C': 'Cruiser','D': 'Destroyer','F': 'Frigate'}
-    for i in range(int(input())):
-        data_id = input().upper()
-        if data.get(data_id) == None:
+    for _ in range(int(input().strip())):
+        data_id = input().strip().upper()
+        if not data.get(data_id, ""):
             continue
-        else:
-            print(data[data_id])
+        print(data[data_id])
 
 if __name__ == "__main__":
     main()
